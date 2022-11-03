c1599f803aa8f9d52152b549ed46cb10f44829e3
xiaodong <2239370543@qq.com>
2022-11-04 01:03:40 +0800

    Modify the parameters

--- celery_demo/celeryconfig.py
+++ celery_demo/celeryconfig.py
@@ -1,6 +1,7 @@
 PASSWORD = ''
-BROKER_URL = 'redis://:{}@43.142.194.76:6379/0'.format(PASSWORD)
-CELERY_RESULT_BACKEND = 'redis://:{}@43.142.194.76:6379/1'.format(PASSWORD)
+HOST = ''
+BROKER_URL = 'redis://:{}@{}/0'.format(PASSWORD, HOST)
+CELERY_RESULT_BACKEND = 'redis://:{}@{}:6379/1'.format(PASSWORD, HOST)
 # 设置时间参照，不设置默认使用的UTC时间
 CELERY_TIMEZONE = 'Asia/Shanghai'
 # 指定任务的序列化
--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
 celery 启动测试案例
 
-### 将celeryconfig中PASSWORD修改为自己的密码
+### 将celeryconfig中PASSWORD修改为自己的密码,ip地址修改为自己的ip地址
 
 ### 启动服务
 
